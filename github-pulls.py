95ea8d4abecb8768a2c46192e4773efb1d8d4cc4
Mark Goddard <mark@stackhpc.com>
2017-10-20 11:13:54 +0100

    Add github-pulls.py, a script for checking open PRs

---
+++ github-pulls.py
@@ -0,0 +1,29 @@
+import argparse
+import getpass
+
+from termcolor import colored
+import github
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username", required=True, help="Github username")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    password = getpass.getpass("Enter Github password: ")
+    g = github.Github(args.username, password)
+    user = g.get_user()
+    for repo in user.get_repos():
+        pulls = repo.get_pulls()
+        pulls = [pull for pull in pulls]
+        if pulls:
+            print colored(repo.full_name, 'red')
+            for pull in pulls:
+                print colored(pull.title, 'green')
+
+
+if __name__ == "__main__":
+    main()
